5cf6a52d7fadaa104ad9cc03ff46716f20eaf04c
Freyr Fridfinnsson <freyr12@ru.is>
2015-07-23 15:34:47 -0400

    about button now works

---
+++ js/AboutController.js
@@ -0,0 +1,8 @@
+angular.module("kulygin").controller("AboutController", [
+  '$scope',
+  '$location',
+  '$rootScope',
+  '$routeParams',
+function($scope, $location, $rootScope, $routeParams) {
+  $scope.Freyr = "Frooze";
+}]);
--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ angular.module('kulygin').config(['$routeProvider',
 function ($routeProvider) {
   $routeProvider
     .when('/frontpage', { templateUrl: 'views/frontpage.html', controller: 'SimpleController' })
+    .when('/about', { templateUrl: 'views/about.html', controller: 'AboutController' })
     .otherwise({
       redirectTo: '/frontpage'
     });
